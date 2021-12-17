7f6c71e21888b191031ad54ca28089c4248b637f
George Baklanov <92201632+sh1z1kz@users.noreply.github.com>
2021-12-18 00:21:41 +0500

    Add files via upload

--- tutorial_4/script.js
+++ tutorial_4/script.js
@@ -1,7 +1,22 @@
-var a = '1';
-var b = '2';
-var c = a + b;
-var d = false;
+var catNames = ['Vanya', 'Sanya', 'Vasya', 'Gosha'];
+var catColor = ['red', 'yellow', 'black', 'white'];
+var catLen = ['15', '16', '17', '18', '19', '20'];
 
-if (a === 1) {console.log('!!!')}
-else {console.log('...')}
+var arrayCats = [];
+
+for (var i = 0; i< random(1,12); i++) {
+	var cat = {
+		name: catNames[random(0, catNames.length -1)],
+		color: catColor[random(0,catColor.length -1)],
+		len: catLen[random(0,catLen.length -1)],
+	};
+	arrayCats.push(cat);
+}
+
+for (var i = 0; i < arrayCats.length; i++) {
+	console.log(arrayCats[i]);
+}
+
+function random(min,max) {
+	return Math.floor(Math.random() * (max-min + 1)) + min;
+}
